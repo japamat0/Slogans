7ff29fbc44677f65e3115d9c03d2cfd7d65ac924
Jason Matthias <jpmatthias90@gmail.com>
2019-07-25 11:30:29 -0700

    Small style refactor

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -32,7 +32,7 @@ const AppWrapper = styled.div`
 const BodyWrapper = styled.div`
   background-color: #e3e3e3;
   width: 100%;
-  height: 100%;
+  min-height: 100vh;
 `;
 
 export default function App() {
