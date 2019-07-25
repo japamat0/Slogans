2b96def8733f44ec6953ec4d8474c2ad25c5b2bb
Jason Matthias <jpmatthias90@gmail.com>
2019-07-25 10:25:16 -0700

    Remove unused to from imports

--- internals/templates/containers/App/index.js
+++ internals/templates/containers/App/index.js
@@ -10,7 +10,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import HomePage from 'containers/HomePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
 
 import GlobalStyle from '../../global-styles';
@@ -19,7 +18,6 @@ export default function App() {
   return (
     <div>
       <Switch>
-        <Route exact path="/" component={HomePage} />
         <Route component={NotFoundPage} />
       </Switch>
       <GlobalStyle />
