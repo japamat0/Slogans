02b31277818833e7b164b2652847140c63d362b3
Jason Matthias <jpmatthias90@gmail.com>
2019-07-29 15:36:56 -0700

    Remove unnecessary reducers from global reducer

--- app/reducers.js
+++ app/reducers.js
@@ -6,15 +6,15 @@ import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
 
 import history from 'utils/history';
-import globalReducer from 'containers/App/reducer';
 import languageProviderReducer from 'containers/LanguageProvider/reducer';
 
 /**
  * Merges the main reducer with the router state and dynamically injected reducers
  */
 export default function createReducer(injectedReducers = {}) {
+  console.log(injectedReducers);
+
   const rootReducer = combineReducers({
-    global: globalReducer,
     language: languageProviderReducer,
     router: connectRouter(history),
     ...injectedReducers,
